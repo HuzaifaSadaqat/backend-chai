60673aa1321c81c33c716c53a7eda47759c82db3
HuzaifaSadaqat <huzaifasadaqat22@gmail.com>
2024-10-15 00:04:00 +0500

    err

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 //routes import
 import userRouter from './routes/user.routes.js'
