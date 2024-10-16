eabbd5efd241400092f5cd5238d7e0e33532a5e6
HuzaifaSadaqat <huzaifasadaqat22@gmail.com>
2024-10-17 01:12:29 +0500

    Login & Logout

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use((req, res, next) => {
+app.use((req, res, next) => {   //log any request made
     console.log(`${req.method} ${req.url}`);
     next();
 });
